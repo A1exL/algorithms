58d2acbf9cc32bb9f115d34ebecb5c8f0a1195d9
Oleksii Leonov <oleksii_leonov1@epam.com>
2025-02-22 17:44:29 +0200

    Merge two sorted lists. More tests

--- src/test/java/merge/two/sorted/lists/MergeTwoSortedListsTest.java
+++ src/test/java/merge/two/sorted/lists/MergeTwoSortedListsTest.java
@@ -9,8 +9,14 @@ class MergeTwoSortedListsTest {
     @Test
     void mergeTwoLists() {
         MergeTwoSortedLists sut = new MergeTwoSortedLists();
-        ListNode result = sut.mergeTwoLists(listNode(-10,-9,-6,-4,1,9,9), listNode(-5,-3,0,7,8,8));
-        assertEquals("[ -10 -9 -6 -5 -4 -3 0 1 7 8 8 9 9 ]", result.toString());
+        ListNode result1 = sut.mergeTwoLists(listNode(-10,-9,-6,-4,1,9,9), listNode(-5,-3,0,7,8,8));
+        assertEquals("[ -10 -9 -6 -5 -4 -3 0 1 7 8 8 9 9 ]", result1.toString());
+
+        ListNode result2 = sut.mergeTwoLists(listNode(1, 2, 4), listNode(1, 3, 5));
+        assertEquals("[ 1 1 2 3 4 5 ]", result2.toString());
+
+        ListNode result3 = sut.mergeTwoLists(listNode(1, 2, 4), listNode(3, 5));
+        assertEquals("[ 1 2 3 4 5 ]", result3.toString());
     }
 
     private ListNode listNode(int... values) {
